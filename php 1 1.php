c0e05ae3c169ee13bb4907180685f60ebc494bad
evellynkrewer <krewerevellyn@gmail.com>
2025-05-14 10:00:24 -0300

    Add files via upload

---
+++ php 1 1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+ 
+<?php
+//Código PHP
+echo "<h1> Primeiro Programa</h1>";
+echo "<h2> Segundo Ano<h2>";
+echo "<h3> Senac <h3>";
+echo "Olá Mundo!";
+?>
+ 
+</body>
+</html>
